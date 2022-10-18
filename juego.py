d8a85c99b3118d02b27d28b0e74e5b44ada11263
Laura Celis <lauracelis436@gmail.com>
2022-10-18 14:23:19 -0500

    Change the variables

--- juego.py
+++ juego.py
@@ -1,27 +1,35 @@
+"""Start of the game"""
 print("Bienvenido a la isla. Tu misión será encontrar el tesoro")
+"""Conditional #1"""
 a = input("¿Derecha o Izquierda?: ")
 if a.upper() == "DERECHA":
     print("Caiste en un agujero- GAME OVER")
 elif a.upper() == "IZQUIERDA":
-    b = input("¿Nadar o esperar?: ")
-    if b.upper() == "NADAR":
+    """Conditional #2"""
+    a = input("¿Nadar o esperar?: ")
+    if a.upper() == "NADAR":
         print("Atacado por una tribu- GAME OVER")
-    elif b.upper() == "ESPERAR":
-        c = input("Elige una puerta Amarilla | Azul | Roja: ")
-        if c.upper() == "AMARILLA": 
+    elif a.upper() == "ESPERAR":
+        """Conditional #3"""
+        a = input("Elige una puerta Amarilla | Azul | Roja: ")
+        if a.upper() == "AMARILLA": 
             print("¡HAZ GANADO!")
-        elif c.upper() == "AZUL": 
+        elif a.upper() == "AZUL": 
             print("Devorado por bestias - GAME OVER")
-        elif c.upper() == "ROJA":
-            c = input("Consigues una persona, ¿Hablar o correr: ")
-            if c.upper() == "HABLAR":
+        elif a.upper() == "ROJA":
+            """Conditional #4"""
+            a = input("Consigues una persona, ¿Hablar o correr: ")
+            if a.upper() == "HABLAR":
                 print("Eres quemado- GAME OVER")
-            elif c.upper() == "CORRER":
-                c = input("Consigues una puerta amarilla, ¿Entrar o esperar?: ")
-                if c.upper() == "ESPERAR":
+            elif a.upper() == "CORRER":
+                """Conditional #5"""
+                a = input("Consigues una puerta amarilla, ¿Entrar o esperar?: ")
+                if a.upper() == "ESPERAR":
                     print("Te alcanzan y eres quemado- GAME OVER") 
-                elif c.upper() == "ENTRAR":
+                elif a.upper() == "ENTRAR":
                     print("¡HAZ GANADO!")
+            """User enters an invalid option"""
         else: print("GAME OVER")
+    """User enters an invalid option"""        
 else: print("Escoge una de las dos opciones")
 
